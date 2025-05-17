5cf90af4387340597736c7ec8b0a08742578f083
Andrey Filippov <androsphilippos@gmail.com>
2025-05-17 23:43:56 +0500

    fix: fix port fastifyMailer

--- src/plugins/fmailer.ts
+++ src/plugins/fmailer.ts
@@ -7,8 +7,8 @@ const fastifyMailer = async (fastify: FastifyInstance): Promise<void> => {
   const defaults = { from: env.MAIL_FROM };
   const transport = {
     host: env.MAIL_HOST,
-    port: 25,
-    secure: false,
+    port: 465,
+    secure: true,
     auth: {
       user: env.MAIL_USER,
       pass: env.MAIL_PASS,
